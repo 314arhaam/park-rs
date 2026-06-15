9d455a1ce673ced3e010f9c02fed35331f5cd6b9
Parham Abbasi <prmbas@gmail.com>
2026-06-16 01:49:44 +0330

    very basic head ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Head {
+        filename: String,
+
+        #[arg(short, long, default_value_t = 5_u32)]
+        num: u32,
+    },
+}
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Head { filename, num } => {
+            let file = std::fs::File::open(std::path::Path::new(&filename))?;
+            let data = parquet::file::reader::SerializedFileReader::new(file)?;
+            let mut i: u32 = 0;
+            for line in data.into_iter() {
+                println!("{:?}", line);
+                i+=1;
+                if i >= num {
+                    break
+                }
+            }
+        }
+    }
+    Ok(())
 }
