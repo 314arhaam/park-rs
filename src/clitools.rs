b7903a30a1fdbecc89034d0c96b89e6d4a2b9106
Parham Abbasi <prmbas@gmail.com>
2026-06-16 02:49:07 +0330

    implement run method on Command enum

--- src/clitools.rs
+++ src/clitools.rs
@@ -1,4 +1,7 @@
 use clap::{Parser, Subcommand};
+use crate::head;
+use crate::info;
+
 
 #[derive(Parser)]
 pub struct Cli {
@@ -14,4 +17,22 @@ pub enum Commands {
         #[arg(short, long, default_value_t = 5_u32)]
         num: u32,
     },
+
+    Info {
+        filename: String,
+    }
+}
+
+impl Commands {
+    pub fn run(&self) -> Result<(), Box<dyn std::error::Error>>{
+        let res = match self {
+            Commands::Head { filename, num } => {
+                head::subcommand(filename, num)?
+            }
+            Commands::Info { filename } => {
+                info::subcommand(filename)?
+            }
+        };
+        Ok(res)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 mod clitools;
 mod head;
+mod info;
 use clap::Parser;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let cli = clitools::Cli::parse();
-    let res = match cli.command {
-        clitools::Commands::Head { filename, num } => {
-            head::subcommand(filename, num)?;
-        }
-    };
+    let res = cli.command.run()?;
     Ok(res)
 }
