80ef9f2e777dc3e59400b21269f7a99aa33306b9
Parham Abbasi <prmbas@gmail.com>
2026-06-16 04:10:23 +0330

    parquet metadata added

--- src/info.rs
+++ src/info.rs
@@ -1,4 +1,11 @@
+use parquet::file::reader::{FileReader, SerializedFileReader};
+use std::fs::File;
+
 pub fn subcommand(filename: &String) -> Result<(), Box<dyn std::error::Error>>{
     println!("{}", filename);
+    let file = File::open(&filename)?;
+    let data = SerializedFileReader::new(file)?;
+    let metadata = data.metadata();
+    println!("{:#?}", metadata.file_metadata().schema().get_fields());
     Ok(())
 }
