e49cb1970b0d6d518a2daa0d819fbe494eea56a8
Parham Abbasi <prmbas@gmail.com>
2026-06-16 22:34:10 +0330

    optimized

--- src/head.rs
+++ src/head.rs
@@ -1,35 +1,25 @@
+use parquet::file::reader::FileReader;
+
 pub fn subcommand(filename: &String, num: &u32, csv: &bool) -> Result<(), Box<dyn std::error::Error>>{
     let file = std::fs::File::open(std::path::Path::new(filename))?;
     let data = parquet::file::reader::SerializedFileReader::new(file)?;
-    let mut i: u32 = 0;
-    if *csv {
-        'row_loop: for line in data.into_iter() {
-            let v = line?;
-            for val in v.into_columns() {
-                match i {
-                    0 => {
-                        print!("{},", val.0);
-                    }
-                    i if i > (*num) => {
-                        break 'row_loop;
-                    }
-                    _ => {
-                        print!("{},", val.1);
-                    }
-                }
-            }
-            println!("");
-            i+=1;
-        }
+    let metadata = data.metadata();
+    let n_rows = metadata.file_metadata().num_rows();
+    let rows_to_take = if (*num) as i64 > n_rows {
+        n_rows as usize
     } else {
-        'row_loop: for line in data.into_iter() {
-            let v = line?;
-            println!("{:?}", v);
-            if i > (*num) {
-                break 'row_loop;
+        (*num) as usize
+    };
+    for line in data.into_iter().take(rows_to_take) {
+        let v = line?;
+        if *csv {
+                for val in v.into_columns() {
+                    print!("{},", val.1);
+                }
+            } else {
+                print!("{:?}", v);
             }
-            i+=1;
-        }        
-    }
+        println!("");
+        }
     Ok(())
 }
