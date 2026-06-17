f215da4d2f3c57127e4198b8c90d2d0d55c84683
Parham Abbasi <prmbas@gmail.com>
2026-06-17 03:35:04 +0330

    tabular form ok

--- src/clitools.rs
+++ src/clitools.rs
@@ -1,8 +1,14 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 use crate::head;
 use crate::tail;
 use crate::info;
 
+#[derive(Clone, ValueEnum)]
+pub enum Format {
+    Csv,
+    Raw,
+    Table,
+}
 
 #[derive(Parser)]
 pub struct Cli {
@@ -18,8 +24,8 @@ pub enum Commands {
         #[arg(short, long, default_value_t = 5_u32)]
         num: u32,
 
-        #[arg(short, long, default_value_t = false)]
-        csv: bool,
+        #[arg(short, long, default_value = "table")]
+        format: Format,
     },
 
     Tail {
@@ -28,8 +34,8 @@ pub enum Commands {
         #[arg(short, long, default_value_t = 5_u32)]
         num: u32,
 
-        #[arg(short, long, default_value_t = false)]
-        csv: bool,
+        #[arg(short, long, default_value = "table")]
+        format: Format,
     },
 
     Info {
@@ -40,14 +46,14 @@ pub enum Commands {
 impl Commands {
     pub fn run(&self) -> Result<(), Box<dyn std::error::Error>>{
         let res = match self {
-            Commands::Head { filename, num, csv } => {
-                head::subcommand(filename, num, csv)?
+            Commands::Head { filename, num, format } => {
+                head::subcommand(filename, num, format)?
             }
             Commands::Info { filename } => {
                 info::subcommand(filename)?
             }
-            Commands::Tail { filename, num, csv } => {
-                tail::subcommand(filename, num, csv)?
+            Commands::Tail { filename, num, format } => {
+                tail::subcommand(filename, num, format)?
             }
         };
         Ok(res)
