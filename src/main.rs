eecccd6162c352b7be60039552319a3519d02a86
Parham Abbasi <prmbas@gmail.com>
2026-06-19 03:38:48 +0330

    threaded profiler ok

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 mod clitools;
 mod commands;
 mod iotools;
+mod profiler;
 use clap::Parser;
+use core::time;
+use std::thread;
+use std::sync::mpsc;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let pid = sysinfo::get_current_pid().unwrap();
+    println!("{}", pid);
+    thread::sleep(std::time::Duration::from_secs(10));
+    let (tx, rx) = mpsc::channel();
+    let mut prof = profiler::profiler::Profiler::new();
+    let handler = thread::spawn(move || {prof.run(rx)});
     let cli = clitools::Cli::parse();
-    cli.command.run()
+    cli.command.run().unwrap();
+    tx.send(true).unwrap();
+    handler.join().unwrap();
+    thread::sleep(std::time::Duration::from_secs(10));
+    Ok(())
 }
