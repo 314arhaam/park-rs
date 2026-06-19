0d56cb9fa2951b7e34a8064addc171cf05b0881b
Parham Abbasi <prmbas@gmail.com>
2026-06-19 03:50:40 +0330

    minor restruct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 mod clitools;
 mod commands;
 mod iotools;
-mod profiler;
+mod utils;
 use clap::Parser;
 use std::thread;
 use std::sync::mpsc;
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let (tx, rx) = mpsc::channel();
-    let mut prof: profiler::profiler::Profiler = profiler::profiler::Profiler::new();
+    let mut prof: utils::profiler::Profiler = utils::profiler::Profiler::new();
     let handler: thread::JoinHandle<()> = thread::spawn(move || {prof.run(rx)});
     let cli: clitools::Cli = clitools::Cli::parse();
     cli.command.run().unwrap();
